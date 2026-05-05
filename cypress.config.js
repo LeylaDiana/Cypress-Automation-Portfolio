9c1366a3ddb39904d3d47cddcd9285c3f94641f2
LeylaDiana <85359213+LeylaDiana@users.noreply.github.com>
2026-05-05 16:55:03 +0800

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
+  video: true, // Force video recording
+  screenshotOnRunFailure: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    defaultCommandTimeout: 2000,
   },
 });
